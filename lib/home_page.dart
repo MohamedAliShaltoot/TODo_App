726f06d34d47206b77288f64c69c795dd63c92a8
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2024-12-31 01:53:03 +0200

    modify UI

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:sqflite_app/DataBase/sqf_lite.dart';
 
@@ -12,47 +11,42 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   SqfLiteDb sqfLiteDb = SqfLiteDb();
 
+  Future<List<Map>> ireadData() async {
+    List<Map> response = await sqfLiteDb.readData("SELECT * FROM 'notes' ");
+    return response;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home Page"),
+        title: const Text("Notes",style: TextStyle(color: Colors.white),),
+        backgroundColor: const Color.fromARGB(255, 36, 8, 84),
       ),
       body: Column(
         children: [
-          TextButton(onPressed: () async {
-         int id =   await sqfLiteDb.insertData("INSERT INTO notes ('title','note') VALUES ('first app','it wasn ot easy bro')");
-            if (kDebugMode) {
-              print(id);
-            }
-          }, child: const Text("Inset Data")),
-          const SizedBox(
-            height: 20,
-          ),
-          TextButton(onPressed: () async {
-           List<Map> response = await sqfLiteDb.readData("SELECT * FROM 'notes' ");
-            if (kDebugMode) {
-              print(response);
-            }
-          }, child: const Text("Read Data")),
-           const SizedBox(
-            height: 20,
-          ),
-          TextButton(onPressed: () async {
-          int response = await sqfLiteDb.deleteData("DELETE FROM 'notes' WHERE id = 1  ");
-            if (kDebugMode) {
-              print(response);
-            }
-          }, child: const Text("delete Data")),
-           const SizedBox(
-            height: 20,
-          ),
-          TextButton(onPressed: () async {
-           int response = await sqfLiteDb.updateData("UPDATE 'notes' SET title = 'new title', note = 'new note' WHERE id = 2  ");
-            if (kDebugMode) {
-              print(response);
-            }
-          }, child: const Text("update Data")),
+          FutureBuilder(
+              future: ireadData(),
+              builder: (context, AsyncSnapshot<List<Map>> snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: ListTile(
+                          title: Text(snapshot.data![index]['title']),
+                          subtitle: Text(snapshot.data![index]['note']),
+
+                        ),
+                      
+                      );
+                    },
+                  );
+                }
+                return const Center(child: CircularProgressIndicator());
+              }),
         ],
       ),
     );
