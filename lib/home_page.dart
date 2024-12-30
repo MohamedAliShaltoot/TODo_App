13df1bcfaa807b6bbb910f68f79ae5ad1260dccb
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2024-12-31 01:34:50 +0200

    testing all methods

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,60 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:sqflite_app/DataBase/sqf_lite.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  SqfLiteDb sqfLiteDb = SqfLiteDb();
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+      body: Column(
+        children: [
+          TextButton(onPressed: () async {
+         int id =   await sqfLiteDb.insertData("INSERT INTO notes ('title','note') VALUES ('first app','it wasn ot easy bro')");
+            if (kDebugMode) {
+              print(id);
+            }
+          }, child: const Text("Inset Data")),
+          const SizedBox(
+            height: 20,
+          ),
+          TextButton(onPressed: () async {
+           List<Map> response = await sqfLiteDb.readData("SELECT * FROM 'notes' ");
+            if (kDebugMode) {
+              print(response);
+            }
+          }, child: const Text("Read Data")),
+           const SizedBox(
+            height: 20,
+          ),
+          TextButton(onPressed: () async {
+          int response = await sqfLiteDb.deleteData("DELETE FROM 'notes' WHERE id = 1  ");
+            if (kDebugMode) {
+              print(response);
+            }
+          }, child: const Text("delete Data")),
+           const SizedBox(
+            height: 20,
+          ),
+          TextButton(onPressed: () async {
+           int response = await sqfLiteDb.updateData("UPDATE 'notes' SET title = 'new title', note = 'new note' WHERE id = 2  ");
+            if (kDebugMode) {
+              print(response);
+            }
+          }, child: const Text("update Data")),
+        ],
+      ),
+    );
   }
 }
